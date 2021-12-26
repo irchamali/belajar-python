918b55addbf21bcac5e87cf85d1633c39921bffb
irchamali <id.irchamali@gmail.com>
2021-12-27 01:10:04 +0700

    Learn python: data types of numbers

---
+++ #2dataTypes/2-numbers.py
@@ -0,0 +1,43 @@
+# Python Numbers
+# There are three numeric types in Python: int, float, complex
+
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+
+# print(z)
+# print(type(z))
+
+# Int
+# Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length
+
+a = 1
+b = 35656222554887711
+c = -3255522
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+# Float
+# Float, or "floating point number" is a number, positive or negative, containing one or more decimals
+
+d = 1.10
+e = 1.0
+f = -35.59
+
+print(type(d))
+print(type(e))
+print(type(f))
+
+# Complex
+# Complex numbers are written with a "j" as the imaginary part
+
+g = 3+5j
+h = 5j
+i = -5j
+
+print(type(g))
+print(type(h))
+print(type(i))
