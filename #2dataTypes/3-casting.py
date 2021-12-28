c2b4278c0702ffea1d32f34963d769e2169f67dd
irchamali <id.irchamali@gmail.com>
2021-12-28 22:29:54 +0700

    Learn python: casting

---
+++ #2dataTypes/3-casting.py
@@ -0,0 +1,27 @@
+# Casting = dilakukan untuk menentukan tipe pada variabel
+# Python adalah bahasa berorientasi objek, dan karena itu menggunakan kelas untuk mendefinisikan tipe data
+# Casting dengan python dilakukan dengan menggunakan fungsi konstruktor:
+
+# int() - membangun bilangan bulat dari literal bilangan bulat, literal float (dengan menghapus semua desimal), atau literal string (menyediakan string mewakili bilangan bulat)
+# float() - membuat bilangan float dari literal integer, literal float, atau literal string (menyediakan string mewakili float atau integer)
+# str() - membangun string dari berbagai tipe data, termasuk string, literal integer dan literal float
+
+a = int(1)   # a will be 1
+b = int(2.8)  # b will be 2
+c = int("3")  # c will be 3
+
+# print(z)
+# ====
+
+d = float(1)     # d will be 1.0
+e = float(2.8)   # e will be 2.8
+f = float("3")   # f will be 3.0
+g = float("4.2")  # g will be 4.2
+
+# ====
+
+h = str("s1")  # h will be 's1'
+i = str(2)    # i will be '2'
+j = str(3.0)  # j will be '3.0'
+
+print(j)
