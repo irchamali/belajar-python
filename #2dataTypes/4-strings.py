7c5cbb2beefcd0718e884a7815faa47c1fe0a4dc
irchamali <id.irchamali@gmail.com>
2021-12-28 22:44:20 +0700

    Learn python: strings

---
+++ #2dataTypes/4-strings.py
@@ -0,0 +1,18 @@
+# String
+# String dalam python dikelilingi oleh tanda kutip tunggal, atau tanda kutip ganda
+# 'cinta' or "kasih"
+
+a = "Hello"
+print(a)
+
+b = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(b)
+
+c = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(c)
